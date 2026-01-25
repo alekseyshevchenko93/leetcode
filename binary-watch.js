0df94148ed811deece775e404924fc673900a0c7
ashevchenko <aleksey.sevcenco@bequant.io>
2026-01-25 16:41:47 +0200

    401. binary watch

---
+++ binary-watch.js
@@ -0,0 +1,53 @@
+// Hours: 4 LEDs on top
+// Minutes: 6 LEDs on bottom
+
+function read() {
+
+}
+
+const readBinaryWatch = function(turnedOn) {
+  const results = [];
+
+  for (let i = 0; i < 12; i++) {
+    for (let j = 0; j < 60; j++) {
+      const count = countBits(i) + countBits(j);
+
+      if (count === turnedOn) {
+        const time = `${i}:${padLeft(j)}`;
+        console.log('time', time);
+        results.push(time);
+        // console.log('time:', i, j, count);
+      }
+    }
+  }
+
+  return results;
+};
+
+function padLeft(num) {
+  return num > 9 ? num : `0${num}`;
+}
+
+function countBits(num) {
+  let count = 0;
+
+  while (num) {
+    count += 1 & num
+    num = num >> 1;
+  }
+
+  return count;
+}
+
+readBinaryWatch(9);
+// console.log(countBits(7));
+
+// Hours:  [8,2,1], [8,2], [8,1], [8], [4,2,1], [4,2], [4,1], [4], [2,1], [2], [1]
+// total: 11
+// 3 LEDS:  [8,2,1], [4,2,1] - 2
+// 2 LEDS: [8,2], [8,1], [4,2], [4,1], [2,1] - 5
+// 1 LEDS [8], [4], [2], [1] - 4
+// max 0-3 bits
+
+
+// Minutes [32,16,8,2,1], []
